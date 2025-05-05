12ee948916ff4c03278d56562c71798c20d13bf6
hpamula <hubertspamula@gmail.com>
2025-05-05 23:47:50 +0200

    improved GUI, changed README and run.sh

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # expressjs_mvc
+
+To run:
+1. Clone this repository
+2. Make sure run.sh has LF line ending
+3. On Linux or in WSL run ./run.sh
+4. Go to http://localhost:3000/
--- run.sh
+++ run.sh
@@ -4,29 +4,29 @@ npm init -y
 npm install express ejs sqlite3 body-parser
 npm install --save-dev nodemon
 
-mkdir -p src/config \
-  src/models \
-  src/services \
-  src/controllers \
-  src/routes \
-  src/views/partials \
-  src/public/css \
-  data
-touch src/server.js \
-  src/config/db.js \
-  src/models/tender.js \
-  src/services/tenderService.js \
-  src/controllers/tenderController.js \
-  src/routes/tenderRoutes.js \
-  src/views/partials/header.ejs \
-  src/views/partials/footer.ejs \
-  src/views/index.ejs \
-  src/views/tenders.ejs \
-  src/views/ended_tenders.ejs \
-  src/views/details.ejs \
-  src/views/add_tender.ejs \
-  src/views/add_offer.ejs \
-  src/public/css/style.css \
-  data/database.sqlite
+# mkdir -p src/config \
+#   src/models \
+#   src/services \
+#   src/controllers \
+#   src/routes \
+#   src/views/partials \
+#   src/public/css \
+#   data
+# touch src/server.js \
+#   src/config/db.js \
+#   src/models/tender.js \
+#   src/services/tenderService.js \
+#   src/controllers/tenderController.js \
+#   src/routes/tenderRoutes.js \
+#   src/views/partials/header.ejs \
+#   src/views/partials/footer.ejs \
+#   src/views/index.ejs \
+#   src/views/tenders.ejs \
+#   src/views/ended_tenders.ejs \
+#   src/views/details.ejs \
+#   src/views/add_tender.ejs \
+#   src/views/add_offer.ejs \
+#   src/public/css/style.css \
+#   data/database.sqlite
 
-# npm run dev
+npm run dev
